080445e6b75c1b7f1f0bd506d6f99a6924926ba9
Max Eden <mx.eden@gmail.com>
2021-01-11 22:24:05 +0300

    watch period reduced;

--- Hi/HiConst.cs
+++ Hi/HiConst.cs
@@ -3,7 +3,7 @@
     public class HiConst
     {
         public static int UdpTimeout = 1000;
-        public static int WatchPeriod = 1000;
+        public static int WatchPeriod = 500;
         public static int SendTimeout = 1000;
         public static int SendDelay   = 100;
         public static int UdpPort     = 5151;
